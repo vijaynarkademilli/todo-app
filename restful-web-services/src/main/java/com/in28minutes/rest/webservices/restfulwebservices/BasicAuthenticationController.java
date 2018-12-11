5754b5c18a2b2c7ada5e574231a1d18ba8444dcb
vijay narkademilli <vijaykumar.narkademilli@gmail.com>
2018-12-11 20:46:10 +0530

    Basic Authentication

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/BasicAuthenticationController.java
@@ -0,0 +1,18 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by madhuri on 02-12-2018.
+ */
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class BasicAuthenticationController {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/basicauth")
+    public AuthenticationBean getMessage() {
+        return new AuthenticationBean(String.format("You are authenticated"));
+    }
+
+
+}
